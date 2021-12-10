b2940e511d61eaefb896226bce64846ff9a69b84
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2021-12-10 00:12:48 -0500

    Add master branch of Gym as required package

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         "cloudpickle>=1.2.0",
         "importlib_metadata>=4.8.1; python_version < '3.8'",
         "mujoco_py>=1.50, <2.0",
+        "git+https://github.com/openai/gym@master",
     ],
     package_data={
         "gym_robotics": [
