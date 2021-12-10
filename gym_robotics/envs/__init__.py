9cee47e3bc36187df77ccf67e89c0f4720f93e40
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2021-12-09 21:43:12 -0500

    Migrate robotics environments

---
+++ gym_robotics/envs/__init__.py
@@ -0,0 +1,14 @@
+from gym_robotics.envs.fetch_env import FetchEnv
+from gym_robotics.envs.fetch.slide import FetchSlideEnv
+from gym_robotics.envs.fetch.pick_and_place import FetchPickAndPlaceEnv
+from gym_robotics.envs.fetch.push import FetchPushEnv
+from gym_robotics.envs.fetch.reach import FetchReachEnv
+
+from gym_robotics.envs.hand.reach import HandReachEnv
+from gym_robotics.envs.hand.manipulate import HandBlockEnv
+from gym_robotics.envs.hand.manipulate import HandEggEnv
+from gym_robotics.envs.hand.manipulate import HandPenEnv
+
+from gym_robotics.envs.hand.manipulate_touch_sensors import HandBlockTouchSensorsEnv
+from gym_robotics.envs.hand.manipulate_touch_sensors import HandEggTouchSensorsEnv
+from gym_robotics.envs.hand.manipulate_touch_sensors import HandPenTouchSensorsEnv
