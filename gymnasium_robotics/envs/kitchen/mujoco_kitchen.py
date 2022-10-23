33ad7ba90120e3c72135e8196929a43837f83b01
Rodrigo Perez-Vicente <r.l.p.v96@gmail.com>
2022-10-23 02:16:44 -0400

    working xml model

---
+++ gymnasium_robotics/envs/kitchen/mujoco_kitchen.py
@@ -0,0 +1,12 @@
+from gymnasium_robotics.envs.kitchen.kitchen_env import KitchenMicrowaveKettleBottomBurnerLightV0
+
+env = KitchenMicrowaveKettleBottomBurnerLightV0()
+
+env.reset()
+print(env.init_qpos)
+
+print(env.observation_space)
+
+obs, done, rew, info = env.step(env.action_space.sample())
+
+print(obs.shape)
