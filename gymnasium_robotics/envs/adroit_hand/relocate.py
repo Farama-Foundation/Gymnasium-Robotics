d1dbe3ef707b8f1ec5a72d447221f7e4952b8fbd
Rodrigo de Lazcano <r.l.p.v96@gmail.com>
2022-11-03 22:21:18 -0400

    Import Adroit Hand environments from D4RL (#43)
    * add assets and initial envs
    
    * adroit door
    
    * init adroit hammer
    
    * working adroit hammer
    
    * adroit pen
    
    * adroit relocate
    
    * fix pytest adroit pen

---
+++ gymnasium_robotics/envs/adroit_hand/relocate.py
@@ -0,0 +1,170 @@
+from os import path
+
+import numpy as np
+from gymnasium import spaces
+from gymnasium.envs.mujoco.mujoco_env import MujocoEnv
+from gymnasium.utils.ezpickle import EzPickle
+
+from gymnasium_robotics.utils.mujoco_utils import MujocoModelNames
+
+
+class AdroitHandRelocateEnv(MujocoEnv, EzPickle):
+    metadata = {
+        "render_modes": [
+            "human",
+            "rgb_array",
+            "depth_array",
+        ],
+        "render_fps": 100,
+    }
+
+    def __init__(self, **kwargs):
+        xml_file_path = path.join(
+            path.dirname(path.realpath(__file__)),
+            "../assets/adroit_hand/adroit_relocate.xml",
+        )
+        observation_space = spaces.Box(
+            low=-np.inf, high=np.inf, shape=(39,), dtype=np.float64
+        )
+
+        MujocoEnv.__init__(
+            self,
+            model_path=xml_file_path,
+            frame_skip=5,
+            observation_space=observation_space,
+            **kwargs
+        )
+        self._model_names = MujocoModelNames(self.model)
+
+        # Override action_space to -1, 1
+        self.action_space = spaces.Box(
+            low=-1.0, high=1.0, dtype=np.float32, shape=self.action_space.shape
+        )
+
+        # change actuator sensitivity
+        self.model.actuator_gainprm[
+            self._model_names.actuator_name2id[
+                "A_WRJ1"
+            ] : self._model_names.actuator_name2id["A_WRJ0"]
+            + 1,
+            :3,
+        ] = np.array([10, 0, 0])
+        self.model.actuator_gainprm[
+            self._model_names.actuator_name2id[
+                "A_FFJ3"
+            ] : self._model_names.actuator_name2id["A_THJ0"]
+            + 1,
+            :3,
+        ] = np.array([1, 0, 0])
+        self.model.actuator_biasprm[
+            self._model_names.actuator_name2id[
+                "A_WRJ1"
+            ] : self._model_names.actuator_name2id["A_WRJ0"]
+            + 1,
+            :3,
+        ] = np.array([0, -10, 0])
+        self.model.actuator_biasprm[
+            self._model_names.actuator_name2id[
+                "A_FFJ3"
+            ] : self._model_names.actuator_name2id["A_THJ0"]
+            + 1,
+            :3,
+        ] = np.array([0, -1, 0])
+
+        self.target_obj_site_id = self._model_names.site_name2id["target"]
+        self.S_grasp_site_id = self._model_names.site_name2id["S_grasp"]
+        self.obj_body_id = self._model_names.body_name2id["Object"]
+        self.act_mean = np.mean(self.model.actuator_ctrlrange, axis=1)
+        self.act_rng = 0.5 * (
+            self.model.actuator_ctrlrange[:, 1] - self.model.actuator_ctrlrange[:, 0]
+        )
+
+        EzPickle.__init__(self, **kwargs)
+
+    def step(self, a):
+        a = np.clip(a, -1.0, 1.0)
+        a = self.act_mean + a * self.act_rng  # mean center and scale
+        self.do_simulation(a, self.frame_skip)
+        obs = self._get_obs()
+        obj_pos = self.data.xpos[self.obj_body_id].ravel()
+        palm_pos = self.data.site_xpos[self.S_grasp_site_id].ravel()
+        target_pos = self.data.site_xpos[self.target_obj_site_id].ravel()
+
+        reward = -0.1 * np.linalg.norm(palm_pos - obj_pos)  # take hand to object
+        if obj_pos[2] > 0.04:  # if object off the table
+            reward += 1.0  # bonus for lifting the object
+            reward += -0.5 * np.linalg.norm(
+                palm_pos - target_pos
+            )  # make hand go to target
+            reward += -0.5 * np.linalg.norm(
+                obj_pos - target_pos
+            )  # make object go to target
+
+        if np.linalg.norm(obj_pos - target_pos) < 0.1:
+            reward += 10.0  # bonus for object close to target
+        if np.linalg.norm(obj_pos - target_pos) < 0.05:
+            reward += 20.0  # bonus for object "very" close to target
+
+        goal_achieved = True if np.linalg.norm(obj_pos - target_pos) < 0.1 else False
+
+        if self.render_mode == "human":
+            self.render()
+
+        return obs, reward, False, False, dict(success=goal_achieved)
+
+    def _get_obs(self):
+        # qpos for hand
+        # xpos for obj
+        # xpos for target
+        qpos = self.data.qpos.ravel()
+        obj_pos = self.data.xpos[self.obj_body_id].ravel()
+        palm_pos = self.data.site_xpos[self.S_grasp_site_id].ravel()
+        target_pos = self.data.site_xpos[self.target_obj_site_id].ravel()
+        return np.concatenate(
+            [qpos[:-6], palm_pos - obj_pos, palm_pos - target_pos, obj_pos - target_pos]
+        )
+
+    def reset_model(self):
+        self.model.body_pos[self.obj_body_id, 0] = self.np_random.uniform(
+            low=-0.15, high=0.15
+        )
+        self.model.body_pos[self.obj_body_id, 1] = self.np_random.uniform(
+            low=-0.15, high=0.3
+        )
+        self.model.site_pos[self.target_obj_site_id, 0] = self.np_random.uniform(
+            low=-0.2, high=0.2
+        )
+        self.model.site_pos[self.target_obj_site_id, 1] = self.np_random.uniform(
+            low=-0.2, high=0.2
+        )
+        self.model.site_pos[self.target_obj_site_id, 2] = self.np_random.uniform(
+            low=0.15, high=0.35
+        )
+
+        self.set_state(self.init_qpos, self.init_qvel)
+
+        return self._get_obs()
+
+    def get_env_state(self):
+        """
+        Get state of hand as well as objects and targets in the scene
+        """
+        qpos = self.data.qpos.ravel().copy()
+        qvel = self.data.qvel.ravel().copy()
+        hand_qpos = qpos[:30]
+        obj_pos = self.data.xpos[self.obj_body_id].ravel()
+        palm_pos = self.data.site_xpos[self.S_grasp_site_id].ravel()
+        target_pos = self.data.site_xpos[self.target_obj_site_id].ravel()
+        return dict(
+            hand_qpos=hand_qpos,
+            obj_pos=obj_pos,
+            target_pos=target_pos,
+            palm_pos=palm_pos,
+            qpos=qpos,
+            qvel=qvel,
+        )
+
+    def viewer_setup(self):
+        assert self.viewer is not None
+        self.viewer.cam.azimuth = 90
+        self.viewer.cam.distance = 1.5
