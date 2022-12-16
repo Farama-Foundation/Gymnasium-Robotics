6d6ff8c464bb22076779d09f0e4560b883f9c4c2
Rodrigo de Lazcano Perez-Vicente <rlazcano@umd.edu>
2022-12-15 23:51:48 -0500

    add pydocstyle pre-commit

--- gymnasium_robotics/core.py
+++ gymnasium_robotics/core.py
@@ -6,12 +6,12 @@ from gymnasium import error
 
 
 class GoalEnv(gym.Env):
-    r"""A goal-based environment. It functions just as any regular Gymnasium environment but it
-    imposes a required structure on the observation_space. More concretely, the observation
-    space is required to contain at least three elements, namely `observation`, `desired_goal`, and
-    `achieved_goal`. Here, `desired_goal` specifies the goal that the agent should attempt to achieve.
-    `achieved_goal` is the goal that it currently achieved instead. `observation` contains the
-    actual observations of the environment as per usual.
+    r"""A goal-based environment.
+
+    It functions just as any regular Gymnasium environment but it imposes a required structure on the observation_space. More concretely,
+    the observation space is required to contain at least three elements, namely `observation`, `desired_goal`, and `achieved_goal`.
+    Here, `desired_goal` specifies the goal that the agent should attempt to achieve. `achieved_goal` is the goal that it currently achieved instead.
+    `observation` contains the actual observations of the environment as per usual.
 
     - :meth:`compute_reward` - Externalizes the reward function by taking the achieved and desired goal, as well as extra information. Returns reward.
     - :meth:`compute_terminated` - Returns boolean termination depending on the achieved and desired goal, as well as extra information.
@@ -19,6 +19,10 @@ class GoalEnv(gym.Env):
     """
 
     def reset(self, seed: Optional[int] = None):
+        """Reset the environment.
+
+        In addition, check if the observation space is correct by inspecting the `observation`, `achieved_goal`, and `desired_goal` keys.
+        """
         super().reset(seed=seed)
         # Enforce that each GoalEnv uses a Goal-compatible observation space.
         if not isinstance(self.observation_space, gym.spaces.Dict):
@@ -35,9 +39,9 @@ class GoalEnv(gym.Env):
 
     @abstractmethod
     def compute_reward(self, achieved_goal, desired_goal, info):
-        """Compute the step reward. This externalizes the reward function and makes
-        it dependent on a desired goal and the one that was achieved. If you wish to include
-        additional rewards that are independent of the goal, you can include the necessary values
+        """Compute the step reward. This externalizes the reward function and makes it dependent on a desired goal and the one that was achieved.
+
+        If you wish to include additional rewards that are independent of the goal, you can include the necessary values
         to derive it in 'info' and compute it accordingly.
 
         Args:
@@ -56,10 +60,12 @@ class GoalEnv(gym.Env):
 
     @abstractmethod
     def compute_terminated(self, achieved_goal, desired_goal, info):
-        """Compute the step termination. Allows to customize the termination states depending on the
-        desired and the achieved goal. If you wish to determine termination states independent of the goal,
-        you can include necessary values to derive it in 'info' and compute it accordingly. The envirtonment reaches
-        a termination state when this state leads to an episode ending in an episodic task thus breaking .
+        """Compute the step termination. Allows to customize the termination states depending on the desired and the achieved goal.
+
+        If you wish to determine termination states independent of the goal, you can include necessary values to derive it in 'info'
+        and compute it accordingly. The envirtonment reaches a termination state when this state leads to an episode ending in an episodic
+        task thus breaking .
+
         More information can be found in: https://farama.org/New-Step-API#theory
 
         Termination states are
@@ -80,11 +86,13 @@ class GoalEnv(gym.Env):
 
     @abstractmethod
     def compute_truncated(self, achieved_goal, desired_goal, info):
-        """Compute the step truncation. Allows to customize the truncated states depending on the
-        desired and the achieved goal. If you wish to determine truncated states independent of the goal,
-        you can include necessary values to derive it in 'info' and compute it accordingly. Truncated states
-        are those that are out of the scope of the Markov Decision Process (MDP) such as time constraints in a
-        continuing task. More information can be found in: https://farama.org/New-Step-API#theory
+        """Compute the step truncation. Allows to customize the truncated states depending on the desired and the achieved goal.
+
+        If you wish to determine truncated states independent of the goal, you can include necessary values to derive it in 'info'
+        and compute it accordingly. Truncated states are those that are out of the scope of the Markov Decision Process (MDP) such
+        as time constraints in a continuing task.
+
+        More information can be found in: https://farama.org/New-Step-API#theory
 
         Args:
             achieved_goal (object): the goal that was achieved during execution
