5658ac6fd6f5833a002ac8de1a9ae4a8f2e110f6
Rodrigo de Lazcano Perez-Vicente <rlazcano@umd.edu>
2022-12-27 11:42:24 -0500

    mamujoco imports

--- gymnasium_robotics/envs/multiagent_mujoco/__init__.py
+++ gymnasium_robotics/envs/multiagent_mujoco/__init__.py
@@ -1,7 +1,6 @@
 """See the [Doc Page](https://robotics.farama.org/envs/fetch/MaMuJoCo)."""
 
-import multiagent_mujoco.mamujoco_v0  # noqa: F401
-from multiagent_mujoco.coupled_half_cheetah import CoupledHalfCheetah  # noqa: F401
-from multiagent_mujoco.many_segment_ant import ManySegmentAntEnv  # noqa: F401
-from multiagent_mujoco.many_segment_swimmer import ManySegmentSwimmerEnv  # noqa: F401
-from multiagent_mujoco.mujoco_multi import MultiAgentMujocoEnv  # noqa: F401
+from gymnasium_robotics.envs.multiagent_mujoco.coupled_half_cheetah import CoupledHalfCheetah  # noqa: F401
+from gymnasium_robotics.envs.multiagent_mujoco.many_segment_ant import ManySegmentAntEnv  # noqa: F401
+from gymnasium_robotics.envs.multiagent_mujoco.many_segment_swimmer import ManySegmentSwimmerEnv  # noqa: F401
+from gymnasium_robotics.envs.multiagent_mujoco.mujoco_multi import MultiAgentMujocoEnv  # noqa: F401
