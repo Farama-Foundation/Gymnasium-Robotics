2a3699a150831d617b3589c65258ca46a866a145
Kallinteris Andreas <kallinteris@protonmail.com>
2022-12-27 21:31:12 +0200

    doc update

--- gymnasium_robotics/envs/multiagent_mujoco/mujoco_multi.py
+++ gymnasium_robotics/envs/multiagent_mujoco/mujoco_multi.py
@@ -43,21 +43,6 @@ class MultiAgentMujocoEnv(pettingzoo.utils.env.ParallelEnv):
 
     # MaMuJoCo (Multi-Agent MuJoCo)
 
-    These environments were introduced in ["FACMAC: Factored Multi-Agent Centralised Policy Gradients"](https://arxiv.org/abs/2003.06709)
-
-    There are 2 types of Environments, included (1) multi-agent factorizations of [Gymansium/MuJoCo](https://gymnasium.farama.org/environments/mujoco/) tasks and (2) new complex MuJoCo tasks meant to me solved with multi-agent Algorithms
-
-    This Represents the first, easy to use Framework for research of agent factorization
-
-    # API
-
-    MaMuJoCo uses the [PettingZoo.ParallelAPI](https://pettingzoo.farama.org/api/parallel/), but also supports a few extra functions
-    - MaMuJoCo.map_local_actions_to_global_action
-    - MaMuJoCo.map_global_action_to_local_actions
-    - MaMuJoCo.map_global_state_to_local_observations
-    - MaMuJoCo.map_local_observation_to_global_state (NOT IMPLEMENTED)
-    - obsk.get_parts_and_edges
-
     # Action Spaces
 
     For (1) the action space shape is the shape of the single agent domain divided by the number of agents
