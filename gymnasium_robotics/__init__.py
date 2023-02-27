deadd84f18fc1ab98ecbeae98b5ec45b9df6fa63
jjshoots <taijunjet@hotmail.com>
2023-02-27 21:14:35 +0000

    black

--- gymnasium_robotics/__init__.py
+++ gymnasium_robotics/__init__.py
@@ -1118,7 +1118,10 @@ try:
     import sys
     from farama_notifications import notifications
 
-    if "gymnasium_robotics" in notifications and __version__ in notifications["gymnasium_robotics"]:
+    if (
+        "gymnasium_robotics" in notifications
+        and __version__ in notifications["gymnasium_robotics"]
+    ):
         print(notifications["gymnasium_robotics"][__version__], file=sys.stderr)
 except Exception:  # nosec
     pass
