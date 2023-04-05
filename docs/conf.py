783fbf35db7c9e43c98f77276666bcd440e88d19
Manuel Goulão <msilvagoulao@gmail.com>
2023-04-05 12:30:34 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -79,14 +79,10 @@ html_theme_options = {
     "gtag": "G-M37ZN7PPMN",
     "image": "img/robotics-github.png",
     "versioning": True,
+    "source_repository": "https://github.com/Farama-Foundation/Gymnasium-Robotics/",
+    "source_branch": "master",
+    "source_directory": "docs/",
 }
-html_context: Dict[str, Any] = {}
-html_context["conf_py_path"] = "/docs/"
-html_context["display_github"] = True
-html_context["github_user"] = "Farama-Foundation"
-html_context["github_repo"] = "Gymnasium-Robotics"
-html_context["github_version"] = "master"
-html_context["slug"] = "gymnasium-robotics"
 
 html_static_path = ["_static"]
 html_css_files = []
