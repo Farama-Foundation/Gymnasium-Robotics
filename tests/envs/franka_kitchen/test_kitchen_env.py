e6efaed2343ed20eced25543752c99e849aa5633
rodrigodelazcano <rperezvicente@farama.org>
2023-09-04 11:03:20 -0400

    finish tests

--- tests/envs/franka_kitchen/test_kitchen_env.py
+++ tests/envs/franka_kitchen/test_kitchen_env.py
@@ -1,33 +1,121 @@
+from copy import deepcopy
+
 import gymnasium as gym
-from gymnasium_robotics.envs.franka_kitchen.kitchen_env import OBS_ELEMENT_INDICES, OBS_ELEMENT_GOALS
+import pytest
+
+from gymnasium_robotics.envs.franka_kitchen.kitchen_env import (
+    OBS_ELEMENT_GOALS,
+    OBS_ELEMENT_INDICES,
+)
+
+TASKS = ["microwave", "kettle"]
 
 
-def test_task_completion():
+@pytest.mark.parametrize(
+    "remove_task_when_completed, terminate_on_tasks_completed",
+    [[True, True], [False, False]],
+)
+def test_task_completion(remove_task_when_completed, terminate_on_tasks_completed):
+    """
+    This test checks the different task completion configurations for the FrankaKitchen-v1 environment.
 
-    # terminate_on_tasks_completed=True and remove_task_when_completed=True
-    env = gym.make('FrankaKitchen-v1', tasks_to_complete=['microwave', 'kettle'])
-    for _ in range(5):
+    The test checks if the info items returned in each step (`tasks_to_complete`, `step_task_completions`, `episode_task_completions`) are correct and correspond
+    to the behavior of the environment configured at initialization with the arguments: `remove_task_when_completed` and `terminate_on_tasks_completed`.
+    """
+    env = gym.make(
+        "FrankaKitchen-v1",
+        tasks_to_complete=TASKS,
+        remove_task_when_completed=remove_task_when_completed,
+        terminate_on_tasks_completed=terminate_on_tasks_completed,
+    )
+    # Test task completion for 3 consecutive episodes
+    for _ in range(3):
+        tasks_to_complete = deepcopy(TASKS)
+        completed_tasks = set()
         _, info = env.reset()
-        assert set(info['tasks_to_complete']) == {'microwave', 'kettle'}
-        assert len(info['step_task_completions']) == 0, ""
-        assert len(info['episode_task_completions']) == 0
+        assert set(info["tasks_to_complete"]) == set(
+            TASKS
+        ), f"The item `tasks_to_complete` returned by info when the environment is reset: {set(info['tasks_to_complete'])}, must be equal to the `task_to_complete` argument used to initialize the environment: {tasks_to_complete}."
+        assert (
+            len(info["step_task_completions"]) == 0
+        ), f"The key `step_task_completions` returned by info when the environment is reset: {set(info['step_task_completions'])}, must be empty."
+        assert (
+            len(info["episode_task_completions"]) == 0
+        ), f"The key `episode_task_completions` returned by info when the environment is reset: {set(info['episode_task_completions'])}, must be empty."
 
-        # force achieved kettle goal
-        env.data.qpos[OBS_ELEMENT_INDICES["kettle"]] = OBS_ELEMENT_GOALS["kettle"]
-        _, _, _, _, info = env.step(env.action_space.sample())
-        assert set(info['tasks_to_complete']) == {'microwave'}
-        assert set(info['step_task_completions']) == {'kettle'}
-        assert set(info['episode_task_completions']) == {'kettle'}
-        
-        # force achieved microwave goal
-        env.data.qpos[OBS_ELEMENT_INDICES["microwave"]] = OBS_ELEMENT_GOALS["microwave"]
-        _, _, terminated, _, info = env.step(env.action_space.sample())
-        assert len(info['tasks_to_complete']) == 0
-        assert set(info['step_task_completions']) == {'microwave'}
-        assert set(info['episode_task_completions']) == {'microwave'}
-        assert terminated
+        terminated = False
 
-    env.close
-# `tasks_to_complete` (list[str]): list of tasks that haven't yet been completed in the current episode.
-#     - `step_task_completions` (list[str]): list of tasks completed in the step taken.
-#     - `episode_task_completions` (list[str]): list of tasks completed during the episode uptil the current step.
+        # Complete a task sequentially for each environment step
+        for task in TASKS:
+            # Force task to be achieved
+            env.data.qpos[OBS_ELEMENT_INDICES[task]] = OBS_ELEMENT_GOALS[task]
+            _, _, terminated, _, info = env.step(env.action_space.sample())
+            completed_tasks.add(task)
+
+            assert set(info["step_task_completions"]) == {
+                task
+            }, f"The key `step_task_completions` returned by info: {set(info['step_task_completions'])}, must be equal to the tasks completed after the current step: {task}."
+            assert (
+                set(info["episode_task_completions"]) == completed_tasks
+            ), f"The key `episode_task_completions` returned by info: {set(info['episode_task_completions'])}, must be equal to the tasks along the current episode: {completed_tasks}."
+            if remove_task_when_completed:
+                tasks_to_complete.remove(task)
+                assert set(info["tasks_to_complete"]) == set(
+                    tasks_to_complete
+                ), f"If environment is initialized with `remove_task_when_completed=True` the item `tasks_to_complete` returned by info: {set(info['tasks_to_complete'])}, must be equal to the tasks that haven't been completed yet: {tasks_to_complete}."
+
+            else:
+                assert set(info["tasks_to_complete"]) == set(
+                    tasks_to_complete
+                ), f"If environment is initialized with `remove_task_when_completed=False` the item `tasks_to_complete` returned by info: {set(info['tasks_to_complete'])}, must be equal to the set of tasks the environment was initialized with: {tasks_to_complete}."
+
+        if terminate_on_tasks_completed:
+            assert (
+                terminated
+            ), "If the environment is initialized with `terminate_on_tasks_complete=True`, the episode must terminate after all tasks are completed."
+        else:
+            assert (
+                not terminated
+            ), "If the environment is initialized with `terminate_on_tasks_complete=False`, the episode must not terminate after all tasks are completed."
+
+    # Complete a task during the same environment step
+    for _ in range(3):
+        tasks_to_complete = deepcopy(TASKS)
+        completed_tasks = set()
+        _, info = env.reset()
+
+        terminated = False
+
+        # Complete a task sequentially for each environment step
+        for task in TASKS:
+            # Force task to be achieved
+            env.data.qpos[OBS_ELEMENT_INDICES[task]] = OBS_ELEMENT_GOALS[task]
+            _, _, terminated, _, info = env.step(env.action_space.sample())
+            completed_tasks.add(task)
+
+        assert (
+            set(info["step_task_completions"]) == completed_tasks
+        ), f"The key `step_task_completions` returned by info: {set(info['step_task_completions'])}, must be equal to the tasks completed after the current step: {completed_tasks}."
+        assert (
+            set(info["episode_task_completions"]) == completed_tasks
+        ), f"The key `episode_task_completions` returned by info: {set(info['episode_task_completions'])}, must be equal to the tasks along the current episode: {completed_tasks}."
+        if remove_task_when_completed:
+            assert (
+                len(info["tasks_to_complete"]) == 0
+            ), f"If environment is initialized with `remove_task_when_completed=True` and all tasks were completed the item `tasks_to_complete` returned by info: {set(info['tasks_to_complete'])}, must be empty."
+
+        else:
+            assert set(info["tasks_to_complete"]) == set(
+                tasks_to_complete
+            ), f"If environment is initialized with `remove_task_when_completed=False` the item `tasks_to_complete` returned by info: {set(info['tasks_to_complete'])}, must be equal to the set of tasks the environment was initialized with: {tasks_to_complete}."
+
+        if terminate_on_tasks_completed:
+            assert (
+                terminated
+            ), "If the environment is initialized with `terminate_on_tasks_complete=True`, the episode must terminate after all tasks are completed."
+        else:
+            assert (
+                not terminated
+            ), "If the environment is initialized with `terminate_on_tasks_complete=False`, the episode must not terminate after all tasks are completed."
+
+    env.close()
