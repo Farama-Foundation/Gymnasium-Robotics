68acc3a59c7d40eb45f905b32bce8691be9947d2
Kallinteris Andreas <30759571+Kallinteris-Andreas@users.noreply.github.com>
2024-10-04 20:30:06 +0300

    Update test_reach.py

--- tests/envs/hand/test_reach.py
+++ tests/envs/hand/test_reach.py
@@ -4,7 +4,7 @@ import gymnasium as gym
 
 
 def test_serialize_deserialize():
-    env1 = gym.make("HandReach-v0", distance_threshold=1e-6)
+    env1 = gym.make("HandReach-v1", distance_threshold=1e-6)
     env1.reset()
     env2 = pickle.loads(pickle.dumps(env1))
 
